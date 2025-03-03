6e9055a5d29b3a0bce1aae4330146a7b9bc9974d
alex daniel baena yepez <alex.baena@est.iudigital.edu.co>
2025-03-02 23:19:21 -0500

    first commit

---
+++ src/main/java/org/example/rollerspeed/RollerSpeedApplication.java
@@ -0,0 +1,15 @@
+package org.example.rollerspeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"org.example"})
+public class RollerSpeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RollerSpeedApplication.class, args);
+    }
+
+}
